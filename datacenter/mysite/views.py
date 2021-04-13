a22b0254389ae5962149da6446b3a620a774f3ee
wayder1205 <mistor60405@gmail.com>
2021-04-13 15:23:00 +0800

    my new site

--- datacenter/mysite/views.py
+++ datacenter/mysite/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def index(request):
 	myname = "黃偉桐"
+	data = [i for i in range(1, 43)]
+	random.shuffle(data)
+	lotto_numbers = data[0:6]
+	special_number = [6]
 	return render(request, 'index.html', locals())
 # Create your views here.
