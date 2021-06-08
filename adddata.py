10918190b6004dc513bda810703d5d054255c6f6
wayder1205 <mistor60405@gmail.com>
2021-06-08 13:48:27 +0800

    fix

--- adddata.py
+++ adddata.py
@@ -6,9 +6,9 @@ django.setup()
 
 from mysite.models import Post
 
-with open("recycle.csv", newline="\n", encoding="utf-8") as csvfile:
+with open("recycle1.csv", newline="\n", encoding="utf-8") as csvfile:
 	rows = csv.reader(csvfile, delimiter=",")
 	for row in rows:
-		print(row[1],row[2])
+		print(row)
 		newdata = Post(title =str(row[1]),body =str(row[2]))
 		newdata.save()
