dca7b172ccf87229b8411967b1db85fc6616c62b
wayder1205 <mistor60405@gmail.com>
2021-06-08 15:23:19 +0800

    country site

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from mysite.models import Post
+from mysite.models import Post, Country, City
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title','pub_date','body')
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Country)
+
+class CityAdmin(admin.ModelAdmin):
+    list_display=('name','population','country')
+
+admin.site.register(City,CityAdmin)
